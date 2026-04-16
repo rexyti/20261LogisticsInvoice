bf7188aa487810fa6b27e9ccd1f31c99cc4c6db2
luifdo <luifsanchez@gmail.com>
2026-04-16 16:57:16 -0500

    implementacion gradle

--- backend/src/main/java/com/logistica/infrastructure/web/handlers/GlobalExceptionHandler.java
+++ backend/src/main/java/com/logistica/infrastructure/web/handlers/GlobalExceptionHandler.java
@@ -1,14 +1,10 @@
 package com.logistica.infrastructure.web.handlers;
 
 import com.logistica.domain.exceptions.ContratoNotFoundException;
-import com.logistica.domain.exceptions.DomainException;
 import com.logistica.domain.exceptions.LiquidacionDuplicadaException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
